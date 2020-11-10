94b061240ff194849c96d19bf2f93d3319dc4ca5
Cristhian Pinheiro <cristhianm.pinheiro@gmail.com>
2020-11-09 23:53:23 -0300

    Implemented Display On Screen and Add Book To library

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-let addNewBook, bookTitle, bookAuthor, bookPages, bookRead, newBook;
-addNewBook = document.querySelector('button[type="submit"]');
+let addNewBook, bookTitle, bookAuthor, bookPages, bookRead, addBookScreen, addBookScreenBtn, backBtn, newBook, body, table, thead, tbody, options;
+addBookScreen = document.querySelector('form');
+addBookScreenBtn = document.querySelector('#add-new-book')
+addBookBtn = document.querySelector('#submitBtn');
+backBtn = document.querySelector('#backBtn');
+body = document.querySelector('body');
+table = document.createElement('table');
+thead = document.createElement('thead');
+tbody = document.querySelector('tbody');
+options = ['title', 'author', 'pages', 'read'];
+
+
 
 let myLibrary = [];
 
@@ -13,27 +23,109 @@ function Books(title, author, pages, read) {
 
 }
 
+
+
+
+
+function createTable(content) {
+
+
+    for (let i = 0; i <= 3; i++) {
+        let th = document.createElement('th');
+        tr.appendChild(th);
+        th.textContent = `${content}`;
+    }
+
+}
+
+
+addBookScreenBtn.addEventListener('click', () => addBookScreen.style.display = 'block');
+backBtn.addEventListener('click', () => addBookScreen.style.display = 'none');
+
+
 function addBookToLibrary() {
 
+    addBookBtn.addEventListener('click', function() {
+    
+        bookTitle = document.querySelector('#book-title').value;
+        bookAuthor = document.querySelector('#book-author').value;
+        bookPages = document.querySelector('#book-pages').value;
+        bookRead = document.querySelectorAll('.book-read');
+        if (bookTitle && bookAuthor && bookPages) {
+            bookRead.forEach(readBtn => {
+                if (readBtn.checked) {
+                    console.log('worked');
+                    newBook = new Books();
+                    newBook.title = bookTitle;
+                    newBook.author = bookAuthor;
+                    newBook.pages = bookPages;
+                    newBook.read = readBtn.value;
 
+                    myLibrary.push(newBook);
+                    displayBookOnPage();
+                    addBookScreen.style.display = 'none';
+
+                }
+            });
+            
+        } else {
+            console.log('11111')
+        }
+        
+    
+    });
     
 }
 
-addNewBook.addEventListener('mouseover', function() {
+function displayBookOnPage() {
+  
+    myLibrary.forEach(eachBook => {
+        let tr = document.createElement('tr');
+        tbody.appendChild(tr);
+
+        for (let i = 0; i <= 3; i++) {
+            let td = document.createElement('td');
+            tr.appendChild(td);
+            td.textContent = `${eachBook[options[i]]}`;
+        
+        }
+        
+        
+        
+
+
+    });
+
+
+}
+
+addBookToLibrary();
+/*addNewBook.addEventListener('mouseover', function() {
     
     bookTitle = document.querySelector('#book-title').value;
     bookAuthor = document.querySelector('#book-author').value;
     bookPages = document.querySelector('#book-pages').value;
     bookRead = document.querySelectorAll('.book-read');
     if (bookTitle && bookAuthor && bookPages) {
-        newBook = new Books(bookTitle, bookAuthor, bookPages, true);
-        myLibrary.push(newBook);
+        bookRead.forEach(readBtn => {
+            if (readBtn.checked) {
+                console.log('worked');
+                newBook.title = bookTitle;
+                newBook.author = bookAuthor;
+                newBook.pages = bookPages;
+                newBook.read = readBtn.value;
+                myLibrary.push(newBook);
+
+                console.log(myLibrary);
+            }
+        });
+        
     } else {
         console.log('11111')
     }
     
 
-});
+});*/
 
 
 
