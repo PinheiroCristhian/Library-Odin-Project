333cf3b0e6d988310ad5e17ed94f4953aa09b675
Cristhian Pinheiro <cristhianm.pinheiro@gmail.com>
2020-11-10 01:51:46 -0300

    Implemented Display On Screen and Add Book To library

--- script.js
+++ script.js
@@ -1,49 +1,30 @@
-let addNewBook, bookTitle, bookAuthor, bookPages, bookRead, addBookScreen, addBookScreenBtn, backBtn, newBook, body, table, thead, tbody, options;
+let addNewBook, bookTitle, bookAuthor, bookPages, bookRead, addBookScreen, addBookScreenBtn, backBtn, newBook, tbody, deleteBtn;
 addBookScreen = document.querySelector('form');
 addBookScreenBtn = document.querySelector('#add-new-book')
 addBookBtn = document.querySelector('#submitBtn');
 backBtn = document.querySelector('#backBtn');
-body = document.querySelector('body');
-table = document.createElement('table');
-thead = document.createElement('thead');
 tbody = document.querySelector('tbody');
-options = ['title', 'author', 'pages', 'read'];
-
-
 
 let myLibrary = [];
 
 
-function Books(title, author, pages, read) {
+function Books(title, author, pages, read, exclude) {
 
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.exclude = exclude;
 
 }
 
-
-
-
-
-function createTable(content) {
-
-
-    for (let i = 0; i <= 3; i++) {
-        let th = document.createElement('th');
-        tr.appendChild(th);
-        th.textContent = `${content}`;
-    }
-
-}
 
 
 addBookScreenBtn.addEventListener('click', () => addBookScreen.style.display = 'block');
 backBtn.addEventListener('click', () => addBookScreen.style.display = 'none');
 
 
-function addBookToLibrary() {
+function addBookToLibrary(display) {
 
     addBookBtn.addEventListener('click', function() {
     
@@ -60,9 +41,12 @@ function addBookToLibrary() {
                     newBook.author = bookAuthor;
                     newBook.pages = bookPages;
                     newBook.read = readBtn.value;
+                    newBook.exclude = 'X';
 
                     myLibrary.push(newBook);
-                    displayBookOnPage();
+                    display();
+                    deleteBook();
+                    myLibrary.pop();
                     addBookScreen.style.display = 'none';
 
                 }
@@ -72,63 +56,52 @@ function addBookToLibrary() {
             console.log('11111')
         }
         
-    
+
     });
     
 }
-
+let id = 1;
 function displayBookOnPage() {
-  
+    
     myLibrary.forEach(eachBook => {
+        
         let tr = document.createElement('tr');
         tbody.appendChild(tr);
-
-        for (let i = 0; i <= 3; i++) {
+        tr.setAttribute('data-id', `${'id' + id}`)
+        
+        for (let key in eachBook) {
             let td = document.createElement('td');
             tr.appendChild(td);
-            td.textContent = `${eachBook[options[i]]}`;
-        
+            if (key === 'exclude') {
+                let span = document.createElement('span');
+                td.appendChild(span);
+                span.textContent = `${eachBook[key]}`;
+                span.setAttribute('data-id', `${'id' + id}`)
+                id++;
+                return;
+            }
+            td.textContent = `${eachBook[key]}`;
         }
         
-        
-        
-
 
     });
 
 
 }
 
-addBookToLibrary();
-/*addNewBook.addEventListener('mouseover', function() {
-    
-    bookTitle = document.querySelector('#book-title').value;
-    bookAuthor = document.querySelector('#book-author').value;
-    bookPages = document.querySelector('#book-pages').value;
-    bookRead = document.querySelectorAll('.book-read');
-    if (bookTitle && bookAuthor && bookPages) {
-        bookRead.forEach(readBtn => {
-            if (readBtn.checked) {
-                console.log('worked');
-                newBook.title = bookTitle;
-                newBook.author = bookAuthor;
-                newBook.pages = bookPages;
-                newBook.read = readBtn.value;
-                myLibrary.push(newBook);
+function deleteBook() {
 
-                console.log(myLibrary);
-            }
+    deleteBtn = document.querySelectorAll('span');
+    
+    deleteBtn.forEach(eachBtn => {
+        eachBtn.addEventListener('click', () => {
+            console.log(eachBtn.dataset.id);
         });
-        
-    } else {
-        console.log('11111')
-    }
-    
-
-});*/
+    });
+}
 
 
-
+addBookToLibrary(displayBookOnPage);
 
 
 
