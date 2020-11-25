b0deede202bc7158908715aa3d05af1a28eb8ec4
Cristhian Pinheiro <cristhianm.pinheiro@gmail.com>
2020-11-24 21:29:12 -0300

    Library remade using class and fixed some bugs

--- script.js
+++ script.js
@@ -1,129 +1,178 @@
-let addNewBook, bookTitle, bookAuthor, bookPages, bookRead, addBookScreen, addBookScreenBtn, backBtn, newBook, tbody, deleteBtn, books, td;
-addBookScreen = document.querySelector('form');
-addBookScreenBtn = document.querySelector('#add-new-book')
-addBookBtn = document.querySelector('#submitBtn');
-backBtn = document.querySelector('#backBtn');
-tbody = document.querySelector('tbody');
-
-let myLibrary = [];
+let id = 0;
+const myLibrary = [
 
 
-function Books(title, author, pages, read, exclude) {
 
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.exclude = exclude;
+];
+
+class Books {
+
+    constructor(bookTitle = 'Unknown', bookAuthor = 'Unknown', bookPages = 0, read) {
+       
+        this.bookTitle = bookTitle;
+        this.bookAuthor = bookAuthor;
+        this.bookPages = bookPages;
+        this.read = read;
+        this.delete = 'X';
+        this.id = id;
+
+    }
 
 }
 
+function start() {
+    showForm();
+    addBook();
+}
 
-
-addBookScreenBtn.addEventListener('click', () => addBookScreen.style.display = 'block');
-backBtn.addEventListener('click', () => addBookScreen.style.display = 'none');
-
-
-function addBookToLibrary(display) {
-
-    addBookBtn.addEventListener('click', function() {
+function htmlElements() {
+    const form = document.querySelector('form');
+    const addBookToLibraryBtn = document.querySelector('#addBookToLibraryBtn');
+    const addBookBtn = document.querySelector('#addBook');
+    const backBtn = document.querySelector('#getBack');
+    const bookName = document.querySelector('#bookTitle').value;
+    const bookAuthor = document.querySelector('#bookAuthor').value;
+    const bookPages = document.querySelector('#bookPage').value;
+    const readBook = document.querySelectorAll('input[type=radio]');
+    const deleteBtn = document.querySelectorAll('.delete');
+    const readBtn = document.querySelectorAll('.read');
     
-        bookTitle = document.querySelector('#book-title').value;
-        bookAuthor = document.querySelector('#book-author').value;
-        bookPages = document.querySelector('#book-pages').value;
-        bookRead = document.querySelectorAll('.book-read');
-        if (bookTitle && bookAuthor && bookPages) {
-            bookRead.forEach(readBtn => {
-                if (readBtn.checked) {
-                    newBook = new Books();
-                    newBook.title = bookTitle;
-                    newBook.author = bookAuthor;
-                    newBook.pages = bookPages;
-                    newBook.read = readBtn.value;
-                    newBook.exclude = 'X';
 
+    return { 
+        form,
+        addBookToLibraryBtn,
+        addBookBtn,
+        backBtn,
+        bookName,
+        bookAuthor,
+        bookPages,
+        readBook,
+        deleteBtn,
+        readBtn
+    }
+}
+
+function showForm() {
+        
+    htmlElements().addBookToLibraryBtn.addEventListener('click', () => {
+        htmlElements().form.style.display = 'block';
+        htmlElements().addBookToLibraryBtn.style.display = 'none';
+    });
+
+    htmlElements().backBtn.addEventListener('click', () => {
+
+        htmlElements().form.style.display = 'none';
+        htmlElements().addBookToLibraryBtn.style.display = 'block';
+
+    });
+    
+}
+
+function addBook() {
+    
+    htmlElements().addBookBtn.addEventListener('click', () => {
+        if (htmlElements().bookName && htmlElements().bookAuthor && htmlElements().bookPages) {
+
+            htmlElements().readBook.forEach(radioBtns => {
+
+                if (radioBtns.checked) {
+                    
+                    const newBook = new Books(htmlElements().bookName, htmlElements().bookAuthor, htmlElements().bookPages, radioBtns.value);
+                    newBook.id = id;
                     myLibrary.push(newBook);
-                    display();
-                    deleteBook();
-                    changeReadStatus();
-                    myLibrary.pop();
-                    addBookScreen.style.display = 'none';
-
                 }
+
             });
-            
-        }
         
+        }
+        htmlElements().form.style.display = 'none';
+        htmlElements().addBookToLibraryBtn.style.display = 'block';
+        displayBooks();
+        deleteBook();
+        changeReadStatus();
 
-    });
-    
-}
-let id = 1;
-function displayBookOnPage() {
-    
-    myLibrary.forEach(eachBook => {
-        
-        let tr = document.createElement('tr');
-        tbody.appendChild(tr);
-        tr.setAttribute('data-id', `${'id' + id}`)
-        
-        for (let key in eachBook) {
-            let td = document.createElement('td');
-            tr.appendChild(td);
-            if (key === 'exclude') {
-                let span = document.createElement('span');
-                td.appendChild(span);
-                span.textContent = `${eachBook[key]}`;
-                span.setAttribute('data-id', `${'id' + id}`)
-                id++;
-                return;
-            }
-            td.textContent = `${eachBook[key]}`;
-        }
-        
 
     });
 
-
+    
 }
 
 function deleteBook() {
 
-    deleteBtn = document.querySelectorAll('span');
-    books = document.querySelectorAll('tr');    
-    deleteBtn.forEach(eachBtn => {
-        eachBtn.addEventListener('click', () => {
-            books.forEach(eachTr => {
-                if (eachBtn.dataset.id === eachTr.dataset.id) {
-                    eachTr.style.display = 'none';
-                }
-            });
-            
+    htmlElements().deleteBtn.forEach(btn => {
 
+        btn.addEventListener('click', () => {
+        
+            myLibrary.forEach(book => {
+
+                if (Number(btn.dataset.id) === Number(book.id)) { //if found a book with the same id as span
+                    myLibrary.splice(myLibrary.indexOf(book), 1);  //it's going to remove it from array
+                    displayBooks(); //then display the leftover books on the page again
+                    return deleteBook();
+                }
+
+            });
+        
         });
+
     });
+    
+    
 }
 
 function changeReadStatus() {
-    td = document.querySelectorAll('td');
-    td.forEach(eachTd => {
-        eachTd.addEventListener('click', () => {
-            if (eachTd.textContent === 'yes') {
-                eachTd.textContent = 'no';
-            } else if (eachTd.textContent === 'no'){
-                eachTd.textContent = 'yes';
-            }
+
+    htmlElements().readBtn.forEach(btn => {
+        btn.addEventListener('click', () => {
+            return btn.textContent === 'yes' ? btn.textContent = 'no' : btn.textContent = 'yes';
         });
+
+    });
+
+
+
+
+}
+
+function displayBooks() {
+
+    const tbody = document.querySelector('tbody');
+    
+    function removeTrs() { //Not duplicate old books when new ones are added
+        const trs = document.querySelectorAll('tbody tr');
+        trs.forEach(tr => {
+            tr.remove();
+        });
+    }
+    removeTrs();
+
+    myLibrary.forEach(book => {
+        const tr = document.createElement('tr');
+        tbody.appendChild(tr);
+        tr.setAttribute('data-id', id);
+        for (let key in book) {
+            const td = document.createElement('td');
+            //tr.appendChild(td);
+            if (key !== 'id') { //it should not display the ID on the table.
+                tr.appendChild(td);
+                if (book[key] === 'yes' || book[key] === 'no') {
+                    td.innerHTML = `<span class="read" data-id="${id}">${book[key]}</span>`
+                }
+                else if (key === 'delete') {
+                    td.innerHTML = `<span class="delete" data-id="${id}">X</span>`;
+                    book.id = id;
+                    id++;
+                } else {
+                    td.textContent = book[key];
+                    
+                }
+                
+            }
+    
+        }
+        
     });
 
 }
 
-
-addBookToLibrary(displayBookOnPage);
-
-
-
-
-
-
-
+start();
